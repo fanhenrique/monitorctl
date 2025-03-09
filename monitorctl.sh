593ca18f66ddf62b98fa6f3354806c78e8e08a55
Henrique Fan <fanhenrique@gmail.com>
2025-03-09 19:48:57 -0300

    monitor control with cron

---
+++ monitorctl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export DISPLAY=$(who | awk '{print $5}' | sed 's/(//;s/)//' | head -n1)
+export XAUTHORITY=$(xauth info | grep "Authority file" | awk '{ print $3 }')
+
+source /usr/local/lib/monitorctl/utils
+
+DAY_BRIGHTNESS=$(get_value "DAY_BRIGHTNESS")
+NIGHT_BRIGHTNESS=$(get_value "NIGHT_BRIGHTNESS")
+
+MONITOR=$(get_value "MONITOR")
+INTERVAL=$(get_value "INTERVAL")
+LOG_FILE=$(get_value "LOG_FILE")
+
+START_NIGHT=$(get_value "START_NIGHT")
+START_NIGHT=$(in_minutes "$START_NIGHT")
+
+END_NIGHT=$(get_value "END_NIGHT")
+END_NIGHT=$(in_minutes "$END_NIGHT")
+
+NOW="$(date +"%H:%M")"
+NOW=$(in_minutes "$NOW")
+
+# Check if it is night or day
+if [[ -n "$NOW" && -n "$START_NIGHT" && -n "$END_NIGHT" ]]; then
+    # [00:00-END_NIGHT]
+    # 00:00 in seconds = 0
+    [[ $NOW -gt 0 && $NOW -lt $END_NIGHT ]] && BRIGHTNESS=$NIGHT_BRIGHTNESS
+    # [END_NIGHT-START_NIGHT]
+    [[ $NOW -gt $END_NIGHT && $NOW -lt $START_NIGHT ]] && BRIGHTNESS=$DAY_BRIGHTNESS
+    # [START_NIGHT-23:59] 
+    # 23:59 in seconds = 1439
+    [[ $NOW -gt $START_NIGHT && $NOW -lt 1439 ]] && BRIGHTNESS=$NIGHT_BRIGHTNESS
+else
+    echo "Error: Time variables not set correctly." >&2
+fi
+
+CURRENT_BRIGHTNESS="$(xrandr --verbose --current | grep "^HDMI-A-0" -A5 | tail -n1 | awk '/Brightness/ {print $2}')"
+
+# Update cron job if configuration file has changed
+command $INTERVAL $LOG_FILE > /dev/null
+
+if [ "$(echo "$CURRENT_BRIGHTNESS > $BRIGHTNESS" | bc -l)" -eq 1 ]; then
+    # Adjust brightness according to config file
+    /usr/local/bin/brightness --brightness $BRIGHTNESS --monitor $MONITOR
+fi
+
+exit 0
---
+++ utils.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+CONFIG_FILE="/home/$(whoami)/.config/monitorctl/config"
+
+get_value() {
+    local variable="$1"
+  grep "^$variable=" "$CONFIG_FILE" | cut -d'=' -f2 | tr -d ' '
+}
+
+in_minutes() {
+    local time="$1"
+
+    # Check if the format is correct (HH:MM)
+    if [[ ! "$time" =~ ^([0-9]{2}):([0-9]{2})$ ]]; then
+        echo "Error: Invalid time format '$time'. Use HH:MM." >&2
+        return 1
+    fi
+
+    local hours=${BASH_REMATCH[1]}
+    local minutes=${BASH_REMATCH[2]}
+
+    # Ensures that hours and minutes are valid numbers
+    if (( hours < 0 || hours > 23 || minutes < 0 || minutes > 59 )); then
+        echo "Error: Time outside valid range (00:00 - 23:59)." >&2
+        return 1
+    fi
+
+    echo $((hours * 60 + minutes))
+}
+
+command(){
+    local interval="$1"
+    local log_file="$2"
+
+    CRON_COMMAND="*/$interval * * * * /usr/local/lib/monitorctl/monitorctl >> /tmp/$log_file 2>&1"
+
+    # (crontab -l; echo "$CRON_COMMAND") | crontab -
+    (crontab -l | grep -v '/usr/local/lib/monitorctl/monitorctl'; echo "$CRON_COMMAND") | crontab -
+
+    echo "New cron job add:"
+    echo "$CRON_COMMAND"
+}
